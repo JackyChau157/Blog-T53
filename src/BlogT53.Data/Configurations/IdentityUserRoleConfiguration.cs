41ca2b1a631ba228f19ebbb409f48066fb359fc1
Chau Le Hoang <chau.le.hoang@stepmediasoftware.com>
2024-05-12 23:06:11 +0700

    lessons 2.7 commit 1

--- src/BlogT53.Data/Configurations/IdentityUserRoleConfiguration.cs
+++ src/BlogT53.Data/Configurations/IdentityUserRoleConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using BlogT53.Core.Domain.Content;
-using BlogT53.Core.Domain.Identity;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
---
+++ src/BlogT53.Data/EF/BlogT53ContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogT53.Data.EF
+{
+    public class BlogT53ContextFactory : IDesignTimeDbContextFactory<BlogT53Context>
+    {
+        public BlogT53Context CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<BlogT53Context>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new BlogT53Context(builder.Options);
+        }
+    }
+}
